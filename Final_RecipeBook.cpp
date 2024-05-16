dac18e6feef85ea80cdba71334435f21fa8839c1
BurglarBarrelRider <126348138+BurglarBarrelRider@users.noreply.github.com>
2024-05-17 00:29:00 +0600

    Update 1
    Created option 1,4,5

---
+++ Final_RecipeBook.cpp
@@ -0,0 +1,122 @@
+
+#include <iostream> // library iostream to use basic data structures
+#include <string> // library string to use classes string
+#include <vector>
+using namespace std;
+
+struct Recipe {
+    string name;
+    string category;
+    vector<string> ingredients;
+    string directions;
+};
+
+void displayRecipe(const Recipe& recipe) { // this function is to display event details after user fill it
+    cout << "Recipe Name: " << recipe.name << endl; 
+    cout << "Category: " << recipe.category << endl;
+    cout << "Ingredients:" << endl;
+    // for (const auto& ingredient : recipe.ingredients) { 
+    //     cout << "- " << ingredient << endl;
+    // }
+    for (int i = 0; i < recipe.ingredients.size(); ++i) {
+        cout << " * " << recipe.ingredients[i] << endl;
+    }
+    cout << "Directions: " << recipe.directions << endl;
+}
+
+void addRecipe(Recipe& recipe) {
+    cout << "------------------------" << endl;
+    cout << "Enter recipe name: ";
+    getline(cin, recipe.name);
+    cout << "Enter recipe category: ";
+    getline(cin, recipe.category);
+
+    int numIngredients;
+    cout << "Enter number of ingredients: ";
+    cin >> numIngredients;
+    cin.ignore(); // используется для очистки буфера ввода после ввода числовых значений с помощью cin.
+    // Это предотвращает нежелательное поведение при использовании getline после cin. Error handling
+
+    recipe.ingredients.clear();
+    for (int i = 0; i < numIngredients; i++) {
+        string ingredient;  
+        // ingredient создается внутри пределов цикла. Каждый раз, когда цикл выполняется, создается новая локальная переменная ingredient
+        cout << "Enter ingredient " << i + 1 << ": ";
+        getline(cin, ingredient);
+        recipe.ingredients.push_back(ingredient); // метод вектора
+    }
+
+    cout << "Enter directions: ";
+    getline(cin, recipe.directions);
+}
+
+void displayAllRecipes(const vector<Recipe>& recipes) {
+    if (recipes.empty()) {
+        cout << "------------------------" << endl;
+        cout << "No recipes found. Add your recipes " << endl;
+        return;
+    }
+
+    for (const auto& recipe : recipes) { // const auto& мы не создаем копию, а ссылку
+        displayRecipe(recipe);
+        cout << "------------------------" << endl;
+    }
+}
+
+// void displayAllRecipes(const vector<Recipe>& recipes) {
+//     if (recipes.empty()) {
+//         cout << "------------------------" << endl;
+//         cout << "No recipes found. Add your recipes " << endl;
+//         return;
+//     }
+
+//     for (auto j = recipes.begin(); j != recipes.end(); ++j) {
+//         const Recipe& recipe = recipes[j - recipes.begin()]; // были ошибки при объявлении recipe=recipes[j - recipes.begin()] 
+//         displayRecipe(recipe);                               // минусуем чтобы достать текущий индекс j элемента recipes            
+//         cout << "------------------------" << endl;
+//     }
+// }
+
+
+int main() {
+    
+    vector<Recipe> recipes;
+
+    char answer = 'Y';
+    while (answer == 'Y' || answer == 'y') {
+        int userSelect;
+        cout << endl;
+        cout << " -------| W e l c o m e |------- "<< endl;
+        cout << "Recipe Collection Organizer:" << endl;
+        cout << "1. Display all recipes" << endl;
+        cout << "3. Delete a recipe by name" << endl;
+        cout << "4. Add a new recipe" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Enter your choice (1-5): ";
+        cin >> userSelect;
+        cin.ignore();
+
+        switch (userSelect) {
+            case 1:
+                displayAllRecipes(recipes);
+                break;
+            case 4: {
+                Recipe newRecipe;
+                addRecipe(newRecipe);
+                recipes.push_back(newRecipe);
+                break;
+            }
+            case 5:
+                cout << "Programm ends here." << endl;
+                return 0;
+            default:
+                cout << "Can not read userSelect. Error/invalid input. Enter a num between 1 and 5 " << endl;
+        }
+
+        cout << "\nDo you want to continue? Y/N: ";
+        cin >> answer;
+        cin.ignore(); // тут тоже были ошибки с инпутом вылетает за границы
+    }
+
+    return 0;
+}
